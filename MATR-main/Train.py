421eb4d07e677bec5f6910e6ad0f4b0209d88429
alirezafay <149387054+alirezafay@users.noreply.github.com>
2024-04-13 00:17:03 +0330

    Update Train.py

--- MATR-main/Train.py
+++ MATR-main/Train.py
@@ -152,8 +152,8 @@ def main():
     joblib.dump(args, 'models/%s/args.pkl' %args.name)
     cudnn.benchmark = True
 
-    folder_dataset_train_ir = "/kaggle/working/MATR/imageFolderDataset/PET/images"
-    folder_dataset_train_vi= "/kaggle/working/MATR/imageFolderDataset/MRI"
+    folder_dataset_train_ir = "/kaggle/working/PET/images"
+    folder_dataset_train_vi= "/kaggle/working/MRI"
 
     transform_train = transforms.Compose([transforms.ToTensor(),
                                           transforms.Normalize((0.485, 0.456, 0.406),
